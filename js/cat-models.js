ab500deb12fe62ce8c0e26b83006c06092626bd5
magicsoultravel-com <camouflagedtiger@proton.me>
2026-06-07 00:26:04 +0200

    asd

--- js/cat-models.js
+++ js/cat-models.js
@@ -1,41 +1,47 @@
 window.CatModels = (() => {
   const SVG = `
-    <svg class="cat-sprite" viewBox="0 0 80 50" width="52" height="34" aria-hidden="true">
+    <svg class="cat-sprite" viewBox="0 0 80 42" width="52" height="28" aria-hidden="true">
       <g class="cat-view-side">
-        <path class="cf cat-tail" d="M14 34 L9 33 L6 28 L7 22 L10 18 L12 21 L13 27 L14 34 Z"/>
-        <path class="cf cat-body" d="M16 32 L20 26 L30 23 L44 23 L54 26 L58 30 L57 34 L52 36 L36 36 L22 35 L16 32 Z"/>
+        <path class="cf cat-tail" d="M16 28 L13 27 L11 25 L10 22 L11 19 L13 20 L15 25 L16 28 Z"/>
+        <path class="cf cat-body" d="M17 28 L21 24 L32 22 L46 23 L54 25 L56 28 L54 31 L40 32 L24 31 L17 28 Z"/>
         <g class="cat-head">
-          <path class="cf" d="M46 24 L50 19 L58 19 L62 24 L62 29 L58 33 L50 33 L46 29 Z"/>
-          <path class="cf" d="M49 19 L51 14 L53 19 Z"/>
-          <path class="cf" d="M57 19 L59 13 L62 20 Z"/>
-          <rect class="cat-eye" x="52" y="24" width="2" height="2" rx="0.3"/>
-          <rect class="cat-eye" x="56" y="24" width="2" height="2" rx="0.3"/>
+          <path class="cf" d="M47 23 L50 19 L57 19 L60 23 L60 27 L57 30 L50 30 L47 27 Z"/>
+          <path class="cf" d="M49 19 L51 15 L53 19 Z"/>
+          <path class="cf" d="M56 19 L58 14 L60 20 Z"/>
+          <rect class="cat-eye" x="52" y="23" width="1.6" height="1.6"/>
+          <rect class="cat-eye" x="55" y="23" width="1.6" height="1.6"/>
         </g>
         <g class="cat-legs">
           <g class="cat-leg cat-leg-a">
-            <path class="cf" d="M22 35 L23 47 L20 50 L26 50 L28 47 L27 35 Z"/>
-            <path class="cf" d="M28 35 L28 46 L25 49 L31 49 L33 46 L32 35 Z"/>
-            <path class="cf" d="M40 35 L39 46 L36 49 L42 49 L44 46 L43 35 Z"/>
-            <path class="cf" d="M48 35 L49 47 L46 50 L52 50 L54 47 L50 35 Z"/>
+            <path class="cf" d="M23 30 L23.5 39 L22.6 41 L24.4 41 L25.1 39 L24.8 30 Z"/>
+            <path class="cf" d="M28 30 L28.5 39 L27.6 41 L29.4 41 L30.1 39 L29.8 30 Z"/>
+            <path class="cf" d="M40 30 L40.5 39 L39.6 41 L41.4 41 L42.1 39 L41.8 30 Z"/>
+            <path class="cf" d="M47 30 L47.5 39 L46.6 41 L48.4 41 L49.1 39 L48.8 30 Z"/>
           </g>
           <g class="cat-leg cat-leg-b">
-            <path class="cf" d="M22 35 L21 46 L18 49 L24 49 L26 46 L25 35 Z"/>
-            <path class="cf" d="M28 35 L29 47 L26 50 L32 50 L34 47 L33 35 Z"/>
-            <path class="cf" d="M40 35 L41 47 L38 50 L44 50 L46 47 L45 35 Z"/>
-            <path class="cf" d="M48 35 L47 46 L44 49 L50 49 L52 46 L51 35 Z"/>
+            <path class="cf" d="M23 30 L23.4 38 L22.5 40 L24.5 40 L25.1 38 L24.7 30 Z"/>
+            <path class="cf" d="M28 30 L28.4 38 L27.5 40 L29.5 40 L30.1 38 L29.7 30 Z"/>
+            <path class="cf" d="M40 30 L40.4 38 L39.5 40 L41.5 40 L42.1 38 L41.7 30 Z"/>
+            <path class="cf" d="M47 30 L47.4 38 L46.5 40 L48.5 40 L49.1 38 L48.7 30 Z"/>
           </g>
         </g>
       </g>
+      <g class="cat-view-lie">
+        <path class="cf cat-tail" d="M12 41 L9 40 L8 38 L9 36 L11 37 L12 41 Z"/>
+        <path class="cf cat-body" d="M14 42 L24 40 L40 39 L56 40 L64 42 L56 41 L40 40 L22 41 L14 42 Z"/>
+        <path class="cf" d="M56 40 L60 38 L64 39 L65 42 L61 42 L56 40 Z"/>
+        <rect class="cat-eye" x="59" y="39" width="1.4" height="1.4"/>
+      </g>
       <g class="cat-view-front">
-        <path class="cf cat-tail" d="M10 32 L8 28 L9 23 L11 21 L12 26 L10 32 Z"/>
-        <path class="cf" d="M22 36 L24 28 L30 24 L38 24 L44 28 L46 36 L42 38 L30 38 L22 36 Z"/>
-        <path class="cf" d="M28 22 L30 16 L34 22 Z"/>
-        <path class="cf" d="M46 22 L48 16 L52 22 Z"/>
-        <rect class="cat-eye" x="31" y="26" width="2" height="2" rx="0.3"/>
-        <rect class="cat-eye" x="37" y="26" width="2" height="2" rx="0.3"/>
-        <path class="cf cat-mouth" d="M32 31 L34 33 L38 33 L40 31 Z"/>
-        <path class="cf" d="M26 36 L25 48 L22 50 L28 50 L30 48 L28 36 Z"/>
-        <path class="cf" d="M50 36 L49 48 L46 50 L52 50 L54 48 L52 36 Z"/>
+        <path class="cf cat-tail" d="M10 29 L9 26 L10 23 L11 25 L10 29 Z"/>
+        <path class="cf" d="M24 32 L26 26 L32 23 L38 23 L42 26 L44 32 L40 33 L30 33 L24 32 Z"/>
+        <path class="cf" d="M29 21 L31 16 L34 21 Z"/>
+        <path class="cf" d="M44 21 L46 16 L49 21 Z"/>
+        <rect class="cat-eye" x="31" y="25" width="1.6" height="1.6"/>
+        <rect class="cat-eye" x="36" y="25" width="1.6" height="1.6"/>
+        <path class="cf cat-mouth" d="M32 28 L34 29.5 L38 29.5 L40 28 Z"/>
+        <path class="cf" d="M28 32 L28.5 39 L27.6 41 L29.4 41 L30.1 39 L29.8 32 Z"/>
+        <path class="cf" d="M48 32 L48.5 39 L47.6 41 L49.4 41 L50.1 39 L49.8 32 Z"/>
       </g>
     </svg>`;
 
